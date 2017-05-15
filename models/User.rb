d3bf72b076ece74191c4a2292090ab43095d34b8
Bertie Wooles <bertie.wooles@hotmail.co.uk>
2017-05-15 15:24:55 +0100

    Adds Completed User model with tests and updated Spec Helper

--- models/User.rb
+++ models/User.rb
@@ -1,18 +1,39 @@
-#require 'dm-migrations'
-#require 'data_mapper'
-#require 'dm-postgres-adapter'
+require 'bcrypt'
+require 'data_mapper'
+require 'dm-postgres-adapter'
+require 'dm-migrations'
 
-#DataMapper::Logger.new($stdout, :debug)
-#DataMapper.setup(:default, "postgres://localhost/bookmark_manager_test_2")
 
 class User
 
-  include DataMapper::Resource
+include DataMapper::Resource
+attr_reader :password
+attr_accessor :password_confirmation
+validates_confirmation_of :password
+
 
 property :id, Serial
-property :title, String
+property :name, String
+property :email, String, required: true, unique: true
+property :password_encrypt, Text
+validates_format_of :email, as: :email_address
+# has n, :spaces, :through => Resource
 
+  def password=(password)
+    @password = password
+    self.password_encrypt = BCrypt::Password.create(password)
+  end
+
+  def self.authenticate(email, password)
+    user = first(email: email)
+    if user && BCrypt::Password.new(user.password_encrypt) == password
+      user
+    else
+      nil
+    end
+  end
 end
 
-#DataMapper.finalize
-#DataMapper.auto_upgrade!
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/makers_bnb_#{ENV['RACK_ENV']}")
+DataMapper.finalize
+DataMapper.auto_upgrade!
