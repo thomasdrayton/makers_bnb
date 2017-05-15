8657bc4dcf8924a5b6f896e4e5df26187ceea00f
Thomas Drayton <tomadrayton@gmail.com>
2017-05-15 12:14:08 +0100

    layout

--- app.rb
+++ app.rb
@@ -3,7 +3,15 @@ require 'sinatra/base'
 class Makers_BNB < Sinatra::Base
 
   get '/' do
-    "Hello"
+    erb: index
+  end
+
+  get '/sign_up' do
+    erb: sign_up
+  end
+
+  get '/sign_in' do
+    erb: sign_in
   end
 
   run! if app_file == $0
