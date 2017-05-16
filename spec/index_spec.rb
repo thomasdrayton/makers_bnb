25edb75ab139264183b93b994676f6e70163776b
Ben Taylor <taylor.ben89@gmail.com>
2017-05-16 15:38:16 +0100

    homepage and tests

--- spec/index_spec.rb
+++ spec/index_spec.rb
@@ -1,12 +1,12 @@
 feature 'index' do
   scenario "Homepage has sign in link" do
     visit ('/')
-    expect(page).to have_link("Sign in")
+    expect(page).to have_link("Sign In")
   end
 
     scenario "Homepage has sign up link" do
       visit ('/')
-      expect(page).to have_link("Sign up")
+      expect(page).to have_link("Sign Up")
     end
 
     scenario "Homepage has search link" do
