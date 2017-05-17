5fe77f0f98f407b2a0c91f29a7ed9cd3a6c50227
Thomas Drayton <tomadrayton@gmail.com>
2017-05-17 10:34:53 +0100

    g

--- spec/Request_spec.rb
+++ spec/Request_spec.rb
@@ -1,20 +1,24 @@
 
 describe Request do
-  let!(:userowner) do
-    User.create(name: 'Bertie', email: 'Test.email@test.com', password: 'Password1234', password_confirmation: 'Password1234')
+
+  before do
+    let!(:userowner) do
+      User.create(name: 'Bertie', email: 'Test.email@test.com', password: 'Password1234', password_confirmation: 'Password1234')
+    end
+
+    let!(:userrenter) do
+      User.create(name: 'Tom', email: 'Tom.email@test.com', password: 'Password1234', password_confirmation: 'Password1234')
+    end
+
+    let!(:space) do
+      Space.create(name: 'Flat', city: 'London', street: 'Comercial Street', postcode: 'E1 6LT', price: 72.06, description: "It's a flat mate", startDate: 15/05/ 2017, endDate: 19/05/2017, user_id: userowner.id)
+    end
+
+    let!(:request) do
+      Request.create(startDateReq: 15/05/2017, endDateReq: 15/05/ 2017, confirmed: false, space_id: space.id, user_id: userrenter.id)
+    end    
   end
 
-  let!(:userrenter) do
-    User.create(name: 'Tom', email: 'Tom.email@test.com', password: 'Password1234', password_confirmation: 'Password1234')
-  end
-
-  let!(:space) do
-    Space.create(name: 'Flat', city: 'London', street: 'Comercial Street', postcode: 'E1 6LT', price: 72.06, description: "It's a flat mate", startDate: 15/05/ 2017, endDate: 19/05/2017, user_id: userowner.id)
-  end
-
-  let!(:request) do
-    Request.create(startDateReq: 15/05/2017, endDateReq: 15/05/ 2017, confirmed: false, space_id: space.id, user_id: userrenter.id)
-  end
 
   # it 'knows who owns the property for the request' do
   #   expect(request.space.user.name).to eq "Bertie"
