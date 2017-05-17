ef26c1434f2b155b1e80c9837a9023b2346361be
Bertie Wooles <bertie.wooles@hotmail.co.uk>
2017-05-17 15:51:17 +0100

    FIXED IT FML

--- spec/sign_up_spec.rb
+++ spec/sign_up_spec.rb
@@ -7,6 +7,6 @@ feature 'User can sign up to Makers_BNB' do
     user = User.last
     expect(user.password_encrypt).to_not eq('Password1234')
     expect(user.name).to eq('Bertie')
-    expect(page).to have_current_path('/')
+    expect(page).to have_current_path("/users/main")
   end
 end
