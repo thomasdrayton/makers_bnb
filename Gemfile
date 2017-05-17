dfaa5b1e4464b82f0432109db8c8eff1556f6c93
Thomas Drayton <tomadrayton@gmail.com>
2017-05-17 16:52:45 +0100

    Basic function of sending text through twilio, needs to be tested and phone number added to new user page

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'sinatra-flash'
 gem 'sinatra-partial'
 gem 'twilio-ruby', '~> 4.11', '>= 4.11.1'
 gem 'envyable'
+gem 'twilio-ruby' 
 
 group :test do
   gem 'capybara'
--- models/Request.rb
+++ models/Request.rb
@@ -1,3 +1,5 @@
+require 'twilio-ruby'
+
 class Request
   include DataMapper::Resource
 
@@ -8,4 +10,21 @@ class Request
   property :startDateReq, Date
   property :endDateReq, Date
   property :confirmed, Boolean
+
+
+  def text_owner_of(space)
+
+    phone_number = space.user.phone.to_s
+
+    account_sid = 'AC11dceb37b4a74ea57004ab32ab0b2837'
+    auth_token = '5c76ce6035906d17b3440a00d58d5c4e'
+
+    client = Twilio::REST::Client.new account_sid, auth_token
+
+    client.account.sms.messages.create(
+    :from => '+441290211655',
+    :to => phone_number,
+    :body => "You have a new request for #{space.name}! Please log in to see the details."
+  end
+
 end
